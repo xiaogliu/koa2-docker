2825e78899d851951a7f4147ece10f466de4392d
Vincent Liu <vincentliu@skyscanner.net>
2021-12-30 17:24:50 +0800

    add api

---
+++ app.js
@@ -0,0 +1,15 @@
+const Koa = require('koa')
+const app = new Koa()
+
+app.use(async (ctx, next) => {
+  if (ctx.request.path === '/api/v1/serverInfo') {
+      ctx.set('Access-Control-Allow-Origin', '*');
+      ctx.response.body = 'Koa version2';
+  } else {
+      await next();
+  }
+});
+
+app.listen(8080, () => {
+  console.log('[demo] request get is starting at port 8080')
+})
